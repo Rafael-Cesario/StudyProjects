2d9844624c53e4ace3865d552eff4796786abf38
Rafael <Rafaeloliveiracds@gmail.com>
2025-12-12 17:28:02 -0300

    Zod validations

--- 03-Authentication/Backend/src/controllers/userController.ts
+++ 03-Authentication/Backend/src/controllers/userController.ts
@@ -1,12 +1,22 @@
 import type { Request, Response } from "express";
 import { userService } from "../services/userService";
+import z from "zod";
 
 class UserController {
   async create(req: Request, res: Response) {
+    const User = z.object({
+      email: z.email(),
+      name: z.string().min(3).max(20),
+      password: z.string(),
+    });
+
+    const user = User.parse(req.body);
+    console.log({ user });
+
     // Tasks
     // Validate user input
     // Create User
-    // Return success message 
+    // Return success message
   }
 }
 
