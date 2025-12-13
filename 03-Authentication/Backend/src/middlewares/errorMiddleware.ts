71f6a290d1ad60e0f6e09f019af6727955c06d45
Rafael <Rafaeloliveiracds@gmail.com>
2025-12-13 12:44:57 -0300

    Catch Prisma errors

--- 03-Authentication/Backend/src/middlewares/errorMiddleware.ts
+++ 03-Authentication/Backend/src/middlewares/errorMiddleware.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import z, { ZodError } from "zod";
+import { Prisma } from "../generated/prisma/client";
 
 export const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof ZodError) {
@@ -16,6 +17,12 @@ export const errorMiddleware = (error: Error, req: Request, res: Response, next:
     return;
   }
 
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2002") res.status(400).json({ error: "Unique constraint failed on the field email." });
+
+    return;
+  }
+
   console.log({ error: error });
-  res.status(500).json({ error: error });
+  res.status(500).json({ error: "An unexpected error occurred" });
 };
