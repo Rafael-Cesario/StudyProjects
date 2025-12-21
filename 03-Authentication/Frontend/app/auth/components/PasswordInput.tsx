b84260c039487db1dcf872ab1a8f29bf45d4a095
Rafael <Rafaeloliveiracds@gmail.com>
2025-12-21 19:57:57 -0300

    Password show/hide state

--- 03-Authentication/Frontend/app/auth/components/PasswordInput.tsx
+++ 03-Authentication/Frontend/app/auth/components/PasswordInput.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
 interface PasswordInputProps {
   props: {
     field: string;
@@ -8,16 +13,23 @@ interface PasswordInputProps {
 }
 
 export const PasswordInput = ({ props: { field, text, value, changeField } }: PasswordInputProps) => {
+  const [hide, setHide] = useState(true);
+
   return (
-    <div className="relative w-100">
+    <div className="relative w-100 flex items-center border border-neutral-800">
       <span className="absolute text-sm text-neutral-400 -top-2.5 left-4 bg-neutral-950 px-4 text-center">{text}</span>
       <input
-        className="w-full py-4 px-8 border border-neutral-800"
-        type="text"
+        className="w-full py-4 px-8"
+        type={hide ? "password" : "text"}
         placeholder={text}
         value={value}
         onChange={(e) => changeField(field, e.target.value)}
       />
+
+      <button className="mr-4" onClick={() => setHide(!hide)}>
+        {hide && <Image width={24} height={24} src="/icons/eyeHide.png" alt={"Eye icon"} />}
+        {hide || <Image width={24} height={24} src="/icons/eyeShow.png" alt={"Eye icon"} />}
+      </button>
     </div>
   );
 };
