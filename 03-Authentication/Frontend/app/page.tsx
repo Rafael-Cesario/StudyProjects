daad6f62593aed920da0311b0c46d909e1089263
Rafael <Rafaeloliveiracds@gmail.com>
2025-12-21 18:06:11 -0300

    Start next

---
+++ 03-Authentication/Frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center mt-20">
+      <h1 className="w-fit text-xl">User authentication</h1>
+      <button className="bg-blue-800 py-2 px-6 cursor-pointer mt-4">Login</button>
+    </div>
+  );
+}
+
