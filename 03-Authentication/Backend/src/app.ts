c49c839ea80b348b311a48d09115a72e329ff42e
Rafael <Rafaeloliveiracds@gmail.com>
2025-12-25 01:29:29 -0300

    Cors

--- 03-Authentication/Backend/src/app.ts
+++ 03-Authentication/Backend/src/app.ts
@@ -1,10 +1,12 @@
 import express, { type Request, type Response } from "express";
 import { userRouter } from "./routes/userRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRouter);
 
