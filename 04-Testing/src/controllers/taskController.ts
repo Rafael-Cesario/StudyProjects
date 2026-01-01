4e0a0abd61ae62b6f3b7fb9575549b5e8ecca990
Rafael <Rafaeloliveiracds@gmail.com>
2026-01-01 01:52:06 -0300

    Create task

--- 04-Testing/src/controllers/taskController.ts
+++ 04-Testing/src/controllers/taskController.ts
@@ -6,9 +6,11 @@ export class TaskController {
   constructor(private taskService: TaskService) {}
 
   async create(req: Request, res: Response) {
-    const Task = z.object({ title: z.string().min(3).max(50), description: z.string() });
-    const task = Task.parse(req.body);
+    const TaskSchema = z.object({ title: z.string().min(3).max(50), description: z.string() });
+    const createTaskData = TaskSchema.parse(req.body);
 
-    res.status(201).json({ task });
+    const task = await this.taskService.create(createTaskData);
+
+    res.status(201).json({ success: true, task });
   }
 }
--- 04-Testing/src/services/taskService.ts
+++ 04-Testing/src/services/taskService.ts
@@ -1,5 +1,14 @@
+import { prisma } from "../prisma";
+
+// Task: Move to interface folder
+interface CreateTask {
+  title: string;
+  description: string;
+}
+
 export class TaskService {
-  async create() {
-    console.log("Task service create...");
+  async create(createTaskData: CreateTask) {
+    const task = await prisma.task.create({ data: createTaskData });
+    return task;
   }
 }
